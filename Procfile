32cf122afb697977471a9f630af1f9f1fc300755
yeondaeun <ydy6@naver.com>
2019-03-07 16:25:00 +0900

    herokustart

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn likelionmock.wsgi --log-file -
--- likelionmock/settings.py
+++ likelionmock/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
